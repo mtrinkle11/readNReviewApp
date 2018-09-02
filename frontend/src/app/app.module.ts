fa936765b211b21bd944202ea940599a225d7e92
Michael Trinkle <mtrinkle11@yahoo.com>
2018-09-02 15:18:16 -0500

    Import FormsModule

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SignInComponent } from './sign-in/sign-in.component';
@@ -33,6 +34,7 @@ import { NavbarComponent } from './navbar/navbar.component';
     AppRoutingModule,
     CalendarModule.forRoot(),
     HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
