8f14606ddde9d314a30ae88796e0c5c158e61890
Michael Trinkle <mtrinkle11@yahoo.com>
2018-09-02 15:19:53 -0500

    Connect login frontend to backend

--- frontend/src/app/sign-in/sign-in.component.ts
+++ frontend/src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  HttpClient,
+  HttpParams,
+  HttpHeaders
+} from '@angular/common/http';
+import {
+  ActivatedRoute
+} from '@angular/router';
+import {
+  Router
+} from "@angular/router";
+
+
 
 @Component({
   selector: 'app-sign-in',
@@ -7,10 +23,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  email: string = '';
+  password: string = '';
+  returnURL: any;
+
+  constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.returnURL = this.route.snapshot.queryParams['returnURL'] || '/calendar';
   }
 
-}
+  login() {
+    const body = new HttpParams()
+      .set('email', this.email)
+      .set('password', this.password);
+    this.http.post('http://localhost:8080/backend/auth.php', body.toString(), {
+      headers: new HttpHeaders()
+        .set('Content-Type', 'application/x-www-form-urlencoded')
+    }).subscribe(data => {
+      if (data === false) {
+        this.email = '';
+        this.password = '';
+      } else {
+        localStorage.setItem('currentUsr', JSON.stringify(data));
+        this.router.navigateByUrl(this.returnURL);
 
+      }
+    });
+  }
+}
