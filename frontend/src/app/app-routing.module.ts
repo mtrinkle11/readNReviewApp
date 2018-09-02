f3312126d5b2d5b8cfa9ad4ca1726da317aff2d0
Michael Trinkle <mtrinkle11@yahoo.com>
2018-09-02 15:17:34 -0500

    AuthGuard used for pages that need to be signed in for access

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 import { CalendarComponent } from "./calendar/calendar.component";
@@ -7,17 +7,20 @@ import { HomeComponent } from "./home/home.component";
 import { ResetPasswordComponent } from "./reset-password/reset-password.component";
 import { SignInComponent } from "./sign-in/sign-in.component";
 import { SubscribeComponent } from "./subscribe/subscribe.component";
-import { PassageComponent } from './passage/passage.component';
+import { PassageComponent } from "./passage/passage.component";
+import { AuthGuard } from './guards/auth.guard';
+import { Router, ActivatedRoute } from '@angular/router';
+
 
 
 const routes: Routes = [
-  { path: "calendar", component: CalendarComponent },
-  { path: "forum", component: ForumComponent },
+  { path: "calendar", component: CalendarComponent, canActivate: [AuthGuard] },
+  { path: "forum", component: ForumComponent, canActivate: [AuthGuard] },
   { path: "", component: HomeComponent },
   { path: "sign-in", component: SignInComponent },
   { path: "subscribe", component: SubscribeComponent },
   { path: "reset-password", component: ResetPasswordComponent },
-  { path: "passage/:id", component: PassageComponent}
+  { path: "passage/:id", component: PassageComponent, canActivate: [AuthGuard]}
 ];
   
   
